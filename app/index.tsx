2632f8fb8aaa4a0b81258d2c1c88f21cc46573c2
alperenozdnc <alperenozdincc@gmail.com>
2025-07-29 03:09:24 +0300

    chore: set everything up firebase, git hooks, formatting, linting, and a bare react native setup

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <View>
+        <Text>Hello World.</Text>
+      </View>
+    </>
+  );
+};
+
+export default Home;
+
+// const styles = StyleSheet.create({});

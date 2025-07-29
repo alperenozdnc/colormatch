52eacbd32ac1f927459f682d6e8ae32126ce68cd
alperenozdnc <alperenozdincc@gmail.com>
2025-07-29 03:11:19 +0300

    chore: set up prettier opinions

---
+++ prettier.config.ts
@@ -0,0 +1,11 @@
+import { type Config } from "prettier";
+
+const config: Config = {
+    trailingComma: "none",
+    tabWidth: 4,
+    semi: true,
+    singleQuote: false,
+};
+
+export default config;
+
